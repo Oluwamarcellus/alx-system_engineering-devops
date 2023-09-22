1e2b5933eda568a0f9799a50a92bce448b689797
Ayowale <haydexwisdom@gmail.com>
2023-09-22 21:12:24 +0100

    Task 0 fiex

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # creates a file in /tmp
 
-file { '/tmp/school':
-  content => 'I Love Puppet',
-  mode    => 0744,
+file {'/tmp/school':
+  path    => '/tmp/school',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet'
 }
