97ba68466774828e8edbd21a773903170a4a36c8
Ayowale <haydexwisdom@gmail.com>
2023-09-22 21:10:45 +0100

    Task 0 fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # creates a file in /tmp
 
-file {'/tmp/school':
-mode => 0744,
-owner => 'www-data',
-group => 'www-data',
-content => 'I Love Puppet',
+file { '/tmp/school':
+  content => 'I Love Puppet',
+  mode    => 0744,
+  owner   => 'www-data',
+  group   => 'www-data',
 }
