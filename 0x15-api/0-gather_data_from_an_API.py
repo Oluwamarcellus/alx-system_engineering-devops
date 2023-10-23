f7f5e84be61e5e50c17c230ec1bedcb80a7e95cd
Ayowale <haydexwisdom@gmail.com>
2023-10-23 23:01:27 +0100

    Task fixed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,28 +5,28 @@ Returns to-do list information for a given employee ID
 import requests
 import sys
 
+if __name__ == "__main__":
+    id_ = int(sys.argv[1])
+    url_u = "https://jsonplaceholder.typicode.com/users"
+    usr_res = requests.get(url_u, params={"id": id_})
+    usr_data = usr_res.json()[0]
 
-id_ = int(sys.argv[1])
-url_u = "https://jsonplaceholder.typicode.com/users"
-usr_res = requests.get(url_u, params={"id": id_})
-usr_data = usr_res.json()[0]
+    url_t = "https://jsonplaceholder.typicode.com/todos"
+    tod_res = requests.get(url_t, params={"userId": id_})
+    todo_data = tod_res.json()
 
-url_t = "https://jsonplaceholder.typicode.com/todos"
-tod_res = requests.get(url_t, params={"userId": id_})
-todo_data = tod_res.json()
+    todo_done = 0
+    for i in todo_data:
+        if i["completed"]:
+            todo_done += 1
 
-todo_done = 0
-for i in todo_data:
-    if i["completed"]:
-        todo_done += 1
+    user_info = "Employee {} is done with tasks({}/{}):".format(
+        usr_data["name"],
+        todo_done,
+        len(todo_data)
+    )
 
-user_info = "Employee {} is done with tasks({}/{}):".format(
-    usr_data["name"],
-    todo_done,
-    len(todo_data)
-)
-
-print(user_info)
-for i in todo_data:
-    if i["completed"]:
-        print("\t {}".format(i["title"]))
+    print(user_info)
+    for i in todo_data:
+        if i["completed"]:
+            print("\t {}".format(i["title"]))
