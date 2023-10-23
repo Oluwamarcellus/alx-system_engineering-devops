abd6bcffff32beb478dd13c0434c5878895eedab
Ayowale <haydexwisdom@gmail.com>
2023-10-23 23:55:49 +0100

    Task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to JSON format."""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w", newline="") as jfile:
+        dt = {user_id: [{
+            "tasks": i["title"],
+            "completed": i["completed"],
+            "username": username
+            } for i in todos]}
+        json.dump(dt, jfile)
