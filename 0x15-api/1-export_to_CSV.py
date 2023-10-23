f25eee61aaff84d0fd2432e500f9916d7c701541
Ayowale <haydexwisdom@gmail.com>
2023-10-23 23:23:45 +0100

    Task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Records all tasks that are owned by the queried employee and return
+the info in a csv file
+"""
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    id_ = int(sys.argv[1])
+    url_u = "https://jsonplaceholder.typicode.com/users"
+    usr_res = requests.get(url_u, params={"id": id_})
+    usr_data = usr_res.json()[0]
+
+    url_t = "https://jsonplaceholder.typicode.com/todos"
+    tod_res = requests.get(url_t, params={"userId": id_})
+    todo_data = tod_res.json()
+
+    with open("{}.csv".format(id_), "w") as file:
+        csv_write = csv.writer(file)
+        for x in todo_data:
+            csv_write.writerow((
+                str(id_),
+                usr_data["username"],
+                x["completed"],
+                x["title"]
+                ))
